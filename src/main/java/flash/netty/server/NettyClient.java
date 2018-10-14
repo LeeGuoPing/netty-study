22ff21c282b8365375b6d534c7b91f756fb115e3
LeeGuoPing <xdliguoping@163.com>
2018-10-14 16:51:35 +0800

    Netty客户端新增重试逻辑

--- src/main/java/flash/netty/server/NettyClient.java
+++ src/main/java/flash/netty/server/NettyClient.java
@@ -6,15 +6,17 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
- * 
  * @author liguoping
+ * @version 1.0
  * @description TODO
  * @time 2018/10/9 18:32
- * @version 1.0
- *
  */
 public class NettyClient {
+    private static final int MAX_RETRY = 5;  // 最大重连次数
 
     public static void main(String[] args) {
         Bootstrap bootstrap = new Bootstrap();
@@ -28,13 +30,29 @@ public class NettyClient {
 
                     }
                 });
-        bootstrap.connect("127.0.0.1",8000).addListener(future -> {
-            if(future.isSuccess()){
-                System.out.println("建立连接成功!");
-            }else{
-                System.out.println("建立连接失败!");
+        connect(bootstrap, "127.0.0.1", 8000, MAX_RETRY);
+    }
+
+    /**
+     * 连接重试逻辑
+     *
+     * @param bootStrap
+     * @param host
+     * @param port
+     */
+    private static void connect(Bootstrap bootStrap, String host, int port, int retry) {
+        bootStrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.out.println("连接次数已经用尽,放弃连接.");
+            } else {
+                int order = (MAX_RETRY - retry) + 1;
+                System.out.println(new Date() + " : " + "重试第: " + order + "次");
+                int delay = 1 << order; // 重试延迟时间
+                bootStrap.config().group().schedule(() -> connect(bootStrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
             }
         });
-
+        return;
     }
 }
